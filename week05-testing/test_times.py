11c8bace92f94645413390432428a509b1aaa2d2
Ayoub <rmapabe@ucl.ac.uk>
2020-12-15 02:55:19 +0000

    negative tests

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -1,30 +1,36 @@
 import times
+import pytest
 
 def test_given_input():
-    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    result = times.compute_overlap_time(large, short)
+    t1 = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    t2 = times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    result = times.compute_overlap_time(t1, t2)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
 
 
 def test_no_overlap():
-    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = times.time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00", 2, 60)
-    result = times.compute_overlap_time(large, short)
+    t1 = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    t2 = times.time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00", 2, 60)
+    result = times.compute_overlap_time(t1, t2)
     expected = []
     assert result == expected   
 
 def test_multiple_overlap():
-    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 13:00:00", 3, 900)
-    short = times.time_range("2010-01-12 10:40:00", "2010-01-12 11:20:00", 2 , 120)
-    result = times.compute_overlap_time(large, short)
+    t1 = times.time_range("2010-01-12 10:00:00", "2010-01-12 13:00:00", 3, 900)
+    t2 = times.time_range("2010-01-12 10:40:00", "2010-01-12 11:20:00", 2 , 120)
+    result = times.compute_overlap_time(t1, t2)
     expected = [('2010-01-12 10:40:00', '2010-01-12 10:50:00'), ('2010-01-12 11:05:00', '2010-01-12 11:20:00')]
     assert result == expected    
 
 def test_touching():
-    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 13:00:00")
-    short = times.time_range("2010-01-12 13:00:00", "2010-01-12 14:00:00")
-    result = times.compute_overlap_time(large, short)
+    t1 = times.time_range("2010-01-12 10:00:00", "2010-01-12 13:00:00")
+    t2 = times.time_range("2010-01-12 13:00:00", "2010-01-12 14:00:00")
+    result = times.compute_overlap_time(t1, t2)
     expected = []
     assert result == expected    
+
+def test_negative():
+    with pytest.raises(ValueError) as e:
+        times.time_range("2010-01-12 10:00:00", "2010-01-12 09:30:00")
+    assert e.match('The end of the time range has to come strictly after its start.')
