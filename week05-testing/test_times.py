f6efb90bd89aaa07996fc01bee32c8cdae63b985
Ayoub <rmapabe@ucl.ac.uk>
2020-12-15 20:02:24 +0000

    parametrization of tests

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -1,6 +1,41 @@
 import times
 import pytest
 
+# with parameterization
+
+@pytest.mark.parametrize("t1, t2, expected",
+[(
+    times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
+    times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60),
+    [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+),
+
+(
+    times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
+    times.time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00", 2, 60),
+    []
+),
+
+(
+    times.time_range("2010-01-12 10:00:00", "2010-01-12 13:00:00", 3, 900),
+    times.time_range("2010-01-12 10:40:00", "2010-01-12 11:20:00", 2 , 120),
+    [('2010-01-12 10:40:00', '2010-01-12 10:50:00'), ('2010-01-12 11:05:00', '2010-01-12 11:20:00')]
+),
+
+(
+    times.time_range("2010-01-12 10:00:00", "2010-01-12 13:00:00"),
+    times.time_range("2010-01-12 13:00:00", "2010-01-12 14:00:00"),
+    []
+)]
+)
+
+def test_all_overlaps(t1,t2,expected):
+    assert times.compute_overlap_time(t1,t2) == expected
+
+"""
+
+# without parameterization
+
 def test_given_input():
     t1 = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     t2 = times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
@@ -28,7 +63,8 @@ def test_touching():
     t2 = times.time_range("2010-01-12 13:00:00", "2010-01-12 14:00:00")
     result = times.compute_overlap_time(t1, t2)
     expected = []
-    assert result == expected    
+    assert result == expected
+"""    
 
 def test_negative():
     with pytest.raises(ValueError) as e:
